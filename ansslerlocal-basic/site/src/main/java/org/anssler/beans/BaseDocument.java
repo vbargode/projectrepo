f75d3b5f961f2a8aea52f850f2e00e622a649d72
Vishal Bargode <vishal.bargode@hayagreeva.co.in>
2017-10-05 23:54:41 +0530

    updated anssler-local

---
+++ ansslerlocal-basic/site/src/main/java/org/anssler/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.anssler.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="ansserlocal:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
